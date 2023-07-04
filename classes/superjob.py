4b8d9344339a7df253c6bf9a0b4427b10304a758
dmitriy.balashov <reaver_std@mail.ru>
2023-07-04 14:09:45 +0500

    add code to superjob class API

--- classes/superjob.py
+++ classes/superjob.py
@@ -18,7 +18,7 @@ class SuperJobAPI:
             'keyword': query,
             'page': 0,
             'count': 100,
-            # 'only_with_salary': True
+            'no_agreement': 1
         }
         response = requests.get(URL, headers=headers, params=params)
         result_page = response.json()
@@ -32,11 +32,8 @@ class SuperJobAPI:
                 self.vacancies.extend(result_page['objects'])
             else:
                 break
-        print(f"Загружено {len(self.vacancies)}")
         return self.vacancies
 
-        # time.strftime('%d.%m.%Y %H:%M', time.gmtime(1688029859))
 
-        # with open("result.json", "w") as json_file:
-        #     json_file.write(json.dumps(self.vacancies, indent=2, ensure_ascii=False))
-        # print(len(self.vacancies))
+# super_job = SuperJobAPI()
+# super_job.get_vacancies("python")
